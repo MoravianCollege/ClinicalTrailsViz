20c99feaf41244b42a2186fd30f5265dd512fe19
kylienorwood <norwoodk@moravian.edu>
2019-06-27 10:44:31 -0400

    Typos fixed, deleted extra files, corrected category with little data

--- R_Scripts/gender_trials.R
+++ R_Scripts/gender_trials.R
@@ -32,7 +32,7 @@ con <- dbConnect(drv,
 dbExecute(con, "SET search_path TO ctgov,public")
 
 # Query gender and display number of completed trials in each in a table
-studies_gender <- dbGetQuery(con, "SELECT * FROM studies NATURAL JOIN eligibilities WHERE overall_status='Completed'")
+studies_gender <- dbGetQuery(con, "SELECT * FROM studies NATURAL JOIN eligibilities WHERE overall_status='Completed' AND gender!=''")
 studies_gender_grouped <- group_by(studies_gender, gender)
 View(summarise(studies_gender_grouped, count=n()), "Gender")
 
@@ -41,9 +41,7 @@ x <- summarise(studies_gender_grouped, count = n())
 x$gender <- factor(x$gender, levels = x$gender[order(-x$count)])
 plot <- ggplot(data=x, aes(x=gender, y=count)) + geom_bar(stat="identity")+ 
   labs(title = "Total Number of Completed Trials by Gender",
-       caption = "The histogram above visulaizes the number of completed trials per the gender of eligibility") + 
+       caption = "The histogram above visualizes the number of completed trials per the gender of eligibility") + 
   theme(plot.title = element_text(hjust = 0.5),
         plot.caption=element_text(hjust=0.5, vjust=0.5))
-
-# Display plot
-plot
+plot + scale_y_continuous(labels = comma)
