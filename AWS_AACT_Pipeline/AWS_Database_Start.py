43f0f61376f77846734103bd742b8d1a64ceb2c5
BAnderson6895 <BAnderson6895@gmail.com>
2019-07-15 15:33:57 -0400

    Reformat DB connection into try block

--- AWS_AACT_Pipeline/AWS_Database_Start.py
+++ AWS_AACT_Pipeline/AWS_Database_Start.py
@@ -5,17 +5,17 @@ import os
 import time
 import subprocess
 
-load_dotenv()
-DBName = 'aact_back'
-DBInstanceIdentifier = os.getenv('DBInstanceIdentifier')
-MasterUsername = os.getenv('MasterUsername')
-MasterUserPassword = os.getenv('MasterUserPassword')
-DBPort = os.getenv('DBPort')
-
-# Create RDS database instance
-rds = boto3.client('rds')
-
 try:
+    load_dotenv()
+    DBName = 'aact_back'
+    DBInstanceIdentifier = os.getenv('DBInstanceIdentifier')
+    MasterUsername = os.getenv('MasterUsername')
+    MasterUserPassword = os.getenv('MasterUserPassword')
+    DBPort = os.getenv('DBPort')
+
+    # Create RDS database instance
+    rds = boto3.client('rds')
+
     response = rds.create_db_instance(
         DBName=DBName,
         DBInstanceIdentifier=DBInstanceIdentifier,
@@ -25,35 +25,35 @@ try:
         Engine='postgres',
         AllocatedStorage=20,
         Port=int(DBPort))
-except Exception as error:
-    print(error)
 
-index = 0
+    index = 0
 
-# get all of the db instances and find index of our instance
-dbs = rds.describe_db_instances()
-for db in dbs['DBInstances']:
-    print(db)
-    if dbs['DBInstances'][index]['DBInstanceIdentifier'] == DBInstanceIdentifier:
-        break
-    else:
-        index = index + 1
-
-# wait until instance is available to get address and populate database
-try:
-    while True:
-        if dbs['DBInstances'][index]['DBInstanceIdentifier'] == DBInstanceIdentifier and \
-                dbs['DBInstances'][index]['DBInstanceStatus'] == 'available':
+    # get all of the db instances and find index of our instance
+    dbs = rds.describe_db_instances()
+    for db in dbs['DBInstances']:
+        print(db)
+        if dbs['DBInstances'][index]['DBInstanceIdentifier'] == DBInstanceIdentifier:
             break
         else:
-            print("Waiting for new instance to become available...")
-            time.sleep(15)
-            dbs = rds.describe_db_instances()
-except Exception as error:
-    print("Cannot locate database index:", error)
+            index = index + 1
 
-try:
+    # wait until instance is available to get address and populate database
+    try:
+        while True:
+            if dbs['DBInstances'][index]['DBInstanceIdentifier'] == DBInstanceIdentifier and \
+                    dbs['DBInstances'][index]['DBInstanceStatus'] == 'available':
+                break
+            else:
+                print("Waiting for new instance to become available...")
+                time.sleep(15)
+                dbs = rds.describe_db_instances()
+    except Exception as error:
+        print("Cannot locate database index:", error)
+
     hostname = dbs['DBInstances'][index]['Endpoint']['Address']
     subprocess.call(['bash', './get_database_data.sh', hostname, DBPort, MasterUsername, DBName])
+
 except Exception as error:
     print(error)
+
+
