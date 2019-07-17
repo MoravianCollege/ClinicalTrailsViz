57e5ecab455123ae1662b2edd5960bd807d32782
kylienorwood <norwoodk@moravian.edu>
2019-07-17 11:46:43 -0400

    Driver added to run the condition type algorithm (run this instead of condition type now)

---
+++ AWS_AACT_Pipeline/src/AWS_AACT_Pipeline/categorize_driver.py
@@ -0,0 +1,25 @@
+from AWS_AACT_Pipeline.src.AWS_AACT_Pipeline.condition_type import ConditionCategorizer
+
+driver = ConditionCategorizer()
+
+print("Starting categorization...")
+
+try:
+    driver.make_connection()
+    print("Connection made successfully")
+
+    driver.delete_table_if_exists()
+    driver.make_data_frame()
+    driver.read_file_conditions()
+
+    driver.categorize()
+    print("Categorization complete")
+
+    driver.make_new_table()
+
+except Exception as error:
+    print(error)
+
+finally:
+    # Closing database connection
+    driver.close_connection()
