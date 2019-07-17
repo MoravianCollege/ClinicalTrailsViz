977ecfcfaee13673092ccc6e4c7200a3ed3b0ad4
BAnderson6895 <BAnderson6895@gmail.com>
2019-07-17 14:50:57 -0400

    Update script call to use parent path reference

--- AWS_AACT_Pipeline/src/AWS_AACT_Pipeline/scripts/generate_tables.sh
+++ AWS_AACT_Pipeline/src/AWS_AACT_Pipeline/scripts/generate_tables.sh
@@ -3,6 +3,10 @@
 #hostname received from create_categorical_tables.py
 hostname=$1
 
-python3 ../../src/AWS_AACT_Pipeline/sponsor_type.py "$hostname"
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+echo ${parent_path}
+cd ${parent_path}
 
-python3 ../../src/AWS_AACT_Pipeline/categorize_driver.py "$hostname"
+python3 ../sponsor_type.py "$hostname"
+
+python3 ../categorize_driver.py "$hostname"
