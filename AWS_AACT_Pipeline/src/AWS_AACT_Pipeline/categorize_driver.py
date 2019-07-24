5fc6e0b6e736794b8020f94a6e647a870a4b972d
kylienorwood <norwoodk@moravian.edu>
2019-07-24 08:47:54 -0400

    Driver now its own class - initializes with a database connection & has method to make tables

--- AWS_AACT_Pipeline/src/AWS_AACT_Pipeline/categorize_driver.py
+++ AWS_AACT_Pipeline/src/AWS_AACT_Pipeline/categorize_driver.py
@@ -1,71 +1,69 @@
-from AWS_AACT_Pipeline.Categorizer import Categorizer
+from AWS_AACT_Pipeline.categorizer import Categorizer
 from AWS_AACT_Pipeline.database_manager import DatabaseManager
 
-database_manager = DatabaseManager()
-categorizer = Categorizer()
 
-conditions = {
-    "original_table": 'conditions',
-    "original_col": 'downcase_name',
-    "new_table": 'condition_type',
-    "new_column": 'condition_category',
-    "json_key_file": 'conditions_key'
-}
+class Driver:
 
-why_stopped = {
-    "original_table": 'studies',
-    "original_col": 'why_stopped',
-    "new_table": 'why_stopped_table',
-    "new_column": 'stop_reason',
-    "json_key_file": 'why_stopped_key'
-}
+    def __init__(self, database_manager):
 
-all_sponsors = {
-    "original_table": 'sponsors',
-    "original_col": 'name',
-    "new_table": 'sponsor_type',
-    "new_column": 'sponsor_category',
-    "json_key_file": 'sponsors_key'
-}
+        self.database_manager = database_manager
+        self.categorizer = Categorizer()
 
-lead_sponsors =  {
-    "original_table": 'sponsors',
-    "original_col": 'name',
-    "new_table": 'lead_sponsor_type',
-    "new_column": 'sponsor',
-    "json_key_file": 'sponsors_key',
-    "extra_sql_command": " WHERE lead_or_collaborator = 'lead'"
-}
-
-list_of_tables = [all_sponsors, lead_sponsors]
-
-print("Starting categorization...")
-
-for table in list_of_tables:
-    try:
-        database_manager.make_connection()
-        print("Connection made successfully")
-    
-        database_manager.delete_table_if_exists(table["new_table"])
+    def make_new_tables(self, original_table, original_col, new_table, new_column, json_key_file, extra_sql_command=''):
+        print("\n")
+        print("Starting categorization...")
+        self.database_manager.delete_table_if_exists(new_table)
         print("Processing data...")
 
-        if "extra_sql_command" in table:
-            database_manager.make_data_frame(table["original_col"],
-                                             table["original_table"],
-                                             table["extra_sql_command"])
+        if "extra_sql_command" is not '':
+            self.database_manager.make_data_frame(original_col, original_table, extra_sql_command)
         else:
-            database_manager.make_data_frame(table["original_col"],
-                                             table["original_table"])
+            self.database_manager.make_data_frame(original_col, original_table)
 
-        categorizer.read_file_conditions(table["json_key_file"])
-    
-        categorizer.categorize(table['original_col'], table['new_column'], database_manager.get_data_frame())
-        database_manager.make_new_table(table['original_col'], table["new_table"], table["new_column"])
+        self.categorizer.read_file_conditions(json_key_file)
+        self.categorizer.categorize(original_col, new_column, self.database_manager.get_data_frame())
+        self.database_manager.make_new_table(original_col, new_table, new_column)
+
+
+if __name__ == '__main__':
+
+    database_conn = DatabaseManager()
+    driver = Driver(database_conn)
+
+    try:
+        database_conn.make_connection()
+        print("Connection made successfully")
+
+        # make new tables for each of our algorithms
+        driver.make_new_tables(original_table="conditions",
+                               original_col="downcase_name",
+                               new_table="condition_type",
+                               new_column="condition_category",
+                               json_key_file="conditions_key")
+
+        driver.make_new_tables(original_table="studies",
+                               original_col="why_stopped",
+                               new_table="why_stopped_table",
+                               new_column="stop_reason",
+                               json_key_file="why_stopped_key")
+
+        driver.make_new_tables(original_table="sponsors",
+                               original_col="name",
+                               new_table="sponsor_type",
+                               new_column="sponsor_category",
+                               json_key_file="sponsors_key")
+
+        driver.make_new_tables(original_table="sponsors",
+                               original_col="name",
+                               new_table="lead_sponsor_type",
+                               new_column="sponsor",
+                               json_key_file="sponsors_key",
+                               extra_sql_command=" WHERE lead_or_collaborator = 'lead'")
 
     except Exception as error:
         print(error)
 
     finally:
         # Closing database connection
-        database_manager.close_connection()
+        database_conn.close_connection()
         print("Categorization complete\n")
