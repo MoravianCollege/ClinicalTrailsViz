b3a76e6394d1b622f594e906a03bb19f809e06cf
kylienorwood <norwoodk@moravian.edu>
2019-07-25 13:07:05 -0400

    Add mock database class to be used for testing

---
+++ AWS_AACT_Pipeline/src/AWS_AACT_Pipeline/mock_db_manager.py
@@ -0,0 +1,27 @@
+
+
+class MockDatabaseManager:
+
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def make_connection(self):
+        pass
+
+    def delete_table_if_exists(self, table):
+        pass
+
+    def make_data_frame(self, original_col, original_table, extra_sql_query=''):
+        pass
+
+    def get_data_frame(self):
+        return self.dataframe
+
+    def make_new_table(self, dataframe, original_col, new_table, new_column):
+        self.final_dataframe = dataframe
+
+    def close_connection(self):
+        pass
+
+    def get_final_dataframe(self):
+        return self.final_dataframe
