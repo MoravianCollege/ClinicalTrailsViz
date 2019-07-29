670c8ffc0de1932dff01ab7cd9d158fa03e7e2fb
BAnderson6895 <BAnderson6895@gmail.com>
2019-07-29 11:52:36 -0400

    Update dbname connection variable to use environment variable

--- R_Scripts/stop_reason_vs_trial_condition_type_proportion.R
+++ R_Scripts/stop_reason_vs_trial_condition_type_proportion.R
@@ -21,7 +21,7 @@ tryCatch({
 # Connect to AACT database
 drv <- dbDriver('PostgreSQL')
 con <- dbConnect(drv,
-                 dbname="aact_back",
+                 dbname=Sys.getenv("dbname"),
                  host=Sys.getenv("host"),
                  port=5432,
                  user=Sys.getenv("userid"),
@@ -35,4 +35,7 @@ dbExecute(con, "SET search_path TO ctgov,public")
 stop <- dbGetQuery(con, "select why_stopped_table.nct_id, why_stopped_table.stop_reason, condition_type.condition_category FROM why_stopped_table JOIN condition_type ON why_stopped_table.nct_id = condition_type.nct_id WHERE why_stopped_table.stop_reason != 'Other'")
 
 # Plot a proportional analysis of trial stop reasons vs trial conditions
-ggplot(df, aes(x = stop.stop_reason, y = ..prop.., group = stop.condition_category)) + geom_bar(aes(fill = stop.condition_category), position = "dodge2") + labs(title="Proportional Analysis of Trial Stop Reason vs Trial Condition Type") + xlab("Stop Reason") + ylab("Proportion (%)") + scale_fill_discrete(name = "Condition Type")
+#ggplot(df, aes(x = stop.stop_reason, y = ..prop.., group = stop.condition_category)) + geom_bar(aes(fill = stop.condition_category), position = "dodge2") + labs(title="Proportional Analysis of Trial Stop Reason vs Trial Condition Type") + xlab("Stop Reason") + ylab("Proportion (%)") + scale_fill_discrete(name = "Condition Type")
+#ggplot(df, aes(x = stop.stop_reason, y = ..prop.., group = stop.condition_category)) + geom_bar(aes(fill = stop.condition_category), position = "dodge2") + labs(title="Proportional Analysis of Trial Stop Reason vs Trial Condition Type") + xlab("Stop Reason") + ylab("Proportion (%)") + scale_fill_discrete(name = "Condition Type") + coord_flip()
+ggplot(df, aes(x = stop.stop_reason, y = ..prop.., group = stop.condition_category)) + geom_bar(aes(fill = stop.condition_category), position = "dodge2") + labs(title="Proportional Analysis of Trial Stop Reason vs Trial Condition Type") + xlab("Stop Reason") + ylab("Proportion (%)") + scale_fill_discrete(name = "Condition Type") + scale_x_discrete(limits = c("Enrollment","Accrual","Sponsor or PI decision","Lack of funding","Ineffective or futile","Safety","Business Decision","Administrative decision","Feasibility reasons","Logistic Reasons","Toxicity")) + coord_flip() + scale_y_continuous(labels=scales::percent)
+#ggplot(df, aes(x = stop.stop_reason, y = ..prop.., group = stop.condition_category)) + geom_bar(aes(fill = stop.condition_category), position = "dodge2") + labs(title="Proportional Analysis of Trial Stop Reason vs Trial Condition Type") + xlab("Stop Reason") + ylab("Proportion (%)") + scale_fill_discrete(name = "Condition Type") + scale_x_discrete(limits = c("Enrollment","Accrual","Sponsor or PI decision","Lack of funding","Ineffective or futile","Safety","Business Decision","Administrative decision","Feasibility reasons","Logistic Reasons","Toxicity")) + coord_flip() + scale_y_continuous(labels=scales::percent) + facet_wrap(~ stop.condition_category, ncol=4)
